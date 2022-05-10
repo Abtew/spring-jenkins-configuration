ea4a1f2c1a628875ab0d7c36cc4e4852f6e9856b
abate.abtew <abate.abtew@gmail.com>
2022-05-10 13:03:37 -0400

    First commit of me

---
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -0,0 +1,24 @@
+package com.javatechie.jenkin.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringJenkinsApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	@GetMapping("/ping")
+	public String message() {
+		return "Wao!! Application Deployed successfully in SAP Cloud..";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
